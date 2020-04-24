971db369b276eec97153f85da966ebcd37af4b75
leejiho <easyho93@gmail.com>
2020-04-24 23:42:26 +0900

    노드버드 공부

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -12,14 +12,21 @@ const initialState = {
 };
 
 export const LOG_IN = 'LOG_IN';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 export const LOG_OUT = 'LOG_OUT';
 export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 
 export const signUpAction = (data) => {
     return {
         type: SIGN_UP,
         data: data,
     }
+}
+
+export const signUpSuccess = {
+    type: SIGN_UP_SUCCESS,
 }
 
 export const loginAction = {
