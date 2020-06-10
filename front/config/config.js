0ca428840f1eae2c3efe8e4dc01f93cd2c296452
leejiho <easyho93@gmail.com>
2020-06-11 00:30:20 +0900

    easyhonodebird.com api.easyhonodebird.com

---
+++ front/config/config.js
@@ -0,0 +1,3 @@
+const backUrl = process.env_NODE_ENV === 'production' ? 'http://api.easyhonodebird.com' : 'http://lopcalhost:3065'
+
+export { backUrl };
--- front/sagas/index.js
+++ front/sagas/index.js
@@ -2,9 +2,10 @@ import { all, call } from 'redux-saga/effects'
 import axios from 'axios';
 import user from './user';
 import post from './post';
+import { backUrl } from '../config/config';
 
 // 불러온 모듈은 모두 공유 되기떄문에 다른 파일(post)도 axios 적용
-axios.defaults.baseURL = 'http://localhost:3065/api';
+axios.defaults.baseURL = `${backUrl}/api`;
 
 export default function* rootSaga() {
     yield all([
