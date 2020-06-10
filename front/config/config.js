b13adffdbfaa2ff86c6f2fbe744921ad74144314
leejiho <easyho93@gmail.com>
2020-06-11 01:09:31 +0900

    config

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
-const backUrl = process.env_NODE_ENV === 'production' ? 'http://api.easyhonodebird.com' : 'http://lopcalhost:3065'
+const backUrl = process.env_NODE_ENV === 'production' ? 'http://api.easyhonodebird.com' : 'http://localhost:3065'
 
 export { backUrl };
