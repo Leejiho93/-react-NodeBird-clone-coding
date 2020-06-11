4e5a4c92be742567adb67f27bf8c73933e43226c
leejiho <easyho93@gmail.com>
2020-06-11 22:58:51 +0900

    decode test

--- lambda/index.js
+++ lambda/index.js
@@ -6,10 +6,10 @@ const S3 = new AWS.S3({ region: 'ap-northeast-2' });
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
-  const filename = Key.split('/')[Key.split('/').length - 1];
+  const filename = decodeURI(Key.split('/')[Key.split('/').length - 1]);
   const ext = Key.split('.')[Key.split('.').length - 1];
-  console.log(Key, filename, ext);
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
+  console.log('key, filename, exit: ', Key, filename, ext);
+  const requiredFormat = ext === 'jpg' || ext ===  'jfif' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
   try {
     const s3Object = await S3.getObject({ // S3에서 이미지를 받아 옵니다.
       Bucket,
